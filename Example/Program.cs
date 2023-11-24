c8dffff4ce140b93fd73e732a00aabe9f73ac45b
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-11-24 16:11:43 +0200

    Add BinaryTree structure

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace Example
+﻿using Library;
+namespace Example
 {
     class Program
     {
         private static void Main(string[] args)
         {
-            Library.List list = new Library.List();
+            List list = new List();
             list.Add(1);
             list.Insert(0, 2);
             list.Add(3);           
@@ -14,7 +15,22 @@
             list.IndexOf(3);
             list.ToArray();
             list.Reverse();
-            
+
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Add(100);
+            binaryTree.Add(58);
+            binaryTree.Add(2);
+            binaryTree.Add(12);
+            binaryTree.Add(33);
+            binaryTree.Add(0);
+
+            binaryTree.Contains(12);
+            binaryTree.Contains(14);
+            binaryTree.ToArray(); 
+
+            Console.WriteLine(binaryTree.Count);
+            Console.WriteLine(binaryTree.Root);
+
             Console.ReadLine();
         }
     }
---
+++ Library/BinaryTree.cs
@@ -0,0 +1,97 @@
+﻿namespace Library
+{
+    public class Node
+    {
+        public int Key;
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public override string ToString()
+        {
+            return Key.ToString();
+        }
+        public Node(int value)
+        {
+            Key = value;
+            Left = Right = null;
+        }
+    }
+
+    public struct BinaryTree
+    {
+        public Node Root { get; private set; }
+        public int Count { get; private set; }
+
+        public void Add(int value)
+        {
+            Root = Add(Root, value);
+        }
+
+        private Node Add(Node root, int value)
+        {
+            if (root == null)
+            {
+                root = new Node(value);
+                Count++;
+                return root;
+            }
+
+            if (value < root.Key)
+            {
+                root.Left = Add(root.Left, value);
+            }
+            else if (value > root.Key)
+            {
+                root.Right = Add(root.Right, value);
+            }
+
+            return root;
+        }
+
+        public bool Contains(int value)
+        {
+            return Contains(Root, value);
+        }
+
+        private bool Contains(Node root, int value)
+        {
+            if (root == null || root.Key == value)
+            {
+                return root != null;
+            }
+
+            if (value < root.Key)
+            {
+                return Contains(root.Left, value);
+            }
+            else
+            {
+                return Contains(root.Right, value);
+            }
+        }
+
+        public void Clear()
+        {
+            Root = null;
+            Count = 0;
+        }
+
+        public int[] ToArray()
+        {
+            int[] result = new int[Count];
+            int index = 0;
+
+            void InorderToArray(Node node)
+            {
+                if (node != null)
+                {
+                    InorderToArray(node.Left);
+                    result[index++] = node.Key;
+                    InorderToArray(node.Right);
+                }
+            }
+
+            InorderToArray(Root);
+            return result;
+        }
+    }
+}
